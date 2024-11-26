42e97eb410ef3bff75f26a97c4d71f6d9d7ecfa2
PetjaPaivarinta <Petja.Paivarinta@proton.me>
2024-11-26 09:18:26 +0200

    all functionality is now implemented for it

--- preload.js
+++ preload.js
@@ -1,12 +1,34 @@
 const os = require('os');
+const path = require('path');
 
-window.addEventListener('DOMContentLoaded', () => {
+function updateSystemInfo() {
+    const homedir = os.homedir();
+    const systemDrive = path.parse(homedir).root;
+
     window.systemInfo = {
         platform: os.platform(),
         arch: os.arch(),
         release: os.release(),
+        machine: os.machine(),
         cpus: os.cpus(),
         totalmem: os.totalmem(),
-        freemem: os.freemem()
+        freemem: os.freemem(),
+        type: os.type(),
+        userInfo: os.userInfo(),
+        homedir: homedir,
+        hostname: os.hostname(),
+        networkInterfaces: os.networkInterfaces(),
+        tempdir: os.tmpdir(),
+        uptime: os.uptime(),
+        version: os.version(),
+        loadavg: os.loadavg(),
+        endianness: os.endianness(),
+        constants: os.constants,
+        systemDrive: systemDrive
     };
+}
+
+window.addEventListener('DOMContentLoaded', () => {
+    updateSystemInfo();
+    setInterval(updateSystemInfo, 1000); // Update every second
 });
